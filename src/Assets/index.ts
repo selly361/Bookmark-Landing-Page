c77febc6f1210956f37c1cd6e8528b7135b6e8d4
selly361 <seikou615@gmail.com>
2023-12-30 19:55:43 +0000

    Add: Export svgs as `ReactComponent`

---
+++ src/Assets/index.ts
@@ -0,0 +1,21 @@
+/* Icons */
+
+export { ReactComponent as ArrowIcon } from "./Icons/arrow-icon.svg"
+export { ReactComponent as CloseIcon } from "./Icons/close-icon.svg"
+export { ReactComponent as ErrorIcon } from "./Icons/error-icon.svg"
+export { ReactComponent as FacebookIcon } from "./Icons/facebook-icon.svg"
+export { ReactComponent as HamburgerIcon } from "./Icons/hamburger-icon.svg"
+
+/* Logos */
+
+export { ReactComponent as BookmarkLogo } from "./Logos/logo-bookmark.svg"
+export { ReactComponent as ChromeLogo } from "./Logos/logo-chrome.svg"
+export { ReactComponent as FirefoxLogo } from "./Logos/logo-firefox.svg"
+export { ReactComponent as OperaLogo } from "./Logos/logo-opera.svg"
+
+/* Tabs */
+
+export { ReactComponent as FeaturesTab1 } from "./Tabs/features-tab-1.svg"
+export { ReactComponent as FeaturesTab2 } from "./Tabs/features-tab-2.svg"
+export { ReactComponent as FeaturesTab3 } from "./Tabs/features-tab-3.svg"
+
