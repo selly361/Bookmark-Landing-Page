2e25558941ded85e1cb7eef609d3739e228ec699
selly361 <seikou615@gmail.com>
2024-01-01 18:29:55 +0000

    Update: Remove tab images export

--- src/Assets/index.ts
+++ src/Assets/index.ts
@@ -12,10 +12,3 @@ export { ReactComponent as BookmarkLogo } from "./Logos/logo-bookmark.svg"
 export { ReactComponent as ChromeLogo } from "./Logos/logo-chrome.svg"
 export { ReactComponent as FirefoxLogo } from "./Logos/logo-firefox.svg"
 export { ReactComponent as OperaLogo } from "./Logos/logo-opera.svg"
-
-/* Tabs */
-
-export { ReactComponent as FeaturesTab1 } from "./Tabs/features-tab-1.svg"
-export { ReactComponent as FeaturesTab2 } from "./Tabs/features-tab-2.svg"
-export { ReactComponent as FeaturesTab3 } from "./Tabs/features-tab-3.svg"
-
