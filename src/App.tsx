be124541adabf53e39577dbed40c941495180390
selly361 <seikou615@gmail.com>
2024-01-01 23:16:20 +0000

    Add: components as a child of `App`

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Header, Hero, Features } from 'Components'
+import { FeaturesProvider } from 'Contexts'
+import { Fragment } from 'react'
 
 function App() {
   return (
-    null
+    <Fragment>
+      <Header />
+      <a href="#features" className="skip-to-content-link">Skip to content</a>
+      <main className='main'>
+        <Hero />
+        <FeaturesProvider>
+          <Features />
+        </FeaturesProvider>
+      </main>
+    </Fragment>
   )
 }
 
